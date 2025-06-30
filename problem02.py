13911832fe58083f162f53373fd04d1d23c4857a
Mukesh kumar <mukeshog14@gmail.com>
2025-06-30 20:53:16 +0530

    Add files via upload

---
+++ problem02.py
@@ -0,0 +1,4 @@
+# write the python program to find the remainder when the number is divided by z
+a=37
+b=3
+print(" remainder vwhen a is divided by b is ; ", a%b)
---
+++ problem04.py
@@ -0,0 +1,4 @@
+# use comparision operator to find out whether a given variable a is hreater than b or not. take a=34 and b=80
+a=int(input("enter number 1 :"))
+b=int(input("enter number 2:"))
+print("a is greater than b is ", a>b)
